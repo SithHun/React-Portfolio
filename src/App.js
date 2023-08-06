ba0e389d3b5dc4dfd40aa764ce760fa5793fbaf5
Seth Sun <sithsun@gmail.com>
2023-08-06 11:56:43 -0400

    Fixed routing issues

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import About from '../pages/About';
 import Portfolio from '../pages/Portfolio';
 import Contact from '../pages/Contact';
 import Resume from '../pages/Resume';
 
 function Content() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/portfolio">
-          <Portfolio />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/resume">
-          <Resume />
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
+    return (
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    );
+  }
 
 export default Content;
