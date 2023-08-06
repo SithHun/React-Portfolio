0358e59dd486d0f13dd59e2f507dce4e55c41656
Seth Sun <sithsun@gmail.com>
2023-08-06 11:47:27 -0400

    Created simple about code

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <img src="developer.jpg" alt="Developer" />
+      <p>A short bio...</p>
+    </div>
+  );
+}
+
+export default About;
