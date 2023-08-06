65a690dddf2f7487b32e562f6ebc90e99da2162e
Seth Sun <sithsun@gmail.com>
2023-08-06 11:48:10 -0400

    coded simple footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/SithHun">GitHub</a>
+      <a href="https://linkedin.com/in/developer">LinkedIn</a>
+      <a href="https://stackoverflow.com/users/developer">Stack Overflow</a>
+    </footer>
+  );
+}
+
+export default Footer;
