a27abfcdb6c6e349c0f0b7880661a8b3458ed3b3
Seth Sun <sithsun@gmail.com>
2023-08-06 19:43:31 -0400

    fixed footer and added footer links to icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import '../assets/Footer.css'
+import githubIcon from '../assets/images/github.svg'
+import linkedinIcon from '../assets/images/linkedin.svg'
+import stackoverflowIcon from '../assets/images/stackoverflow.svg'
 
 function Footer() {
   return (
-    <footer>
-      <a href="https://github.com/SithHun">GitHub</a>
-      <a href="https://linkedin.com/in/developer">LinkedIn</a>
-      <a href="https://stackoverflow.com/users/developer">Stack Overflow</a>
+    <div className='footer-container flexWrapper sm-flexWrapper'>
+    <footer className='footer'>
+      <a href="https://github.com/SithHun">
+        <img src={githubIcon} alt="GitHub"/>
+      </a>
+      <a href="https://linkedin.com/in/developer">
+        <img src={linkedinIcon} alt="LinkedIn"/>
+      </a>
+      <a href="https://stackoverflow.com/users/developer">
+        <img src={stackoverflowIcon} alt="Stack Overflow"/>
+      </a>
     </footer>
+    </div>
   );
 }
 
