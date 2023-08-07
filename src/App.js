b68095fbee336cfb6eb1718a58bb986ae232d0d5
Seth Sun <sithsun@gmail.com>
2023-08-06 22:50:58 -0400

    moved routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
-    <div className='site'>
-    <div className='site-content'>
-    <Router>
-      <div className="App">
-        <Header />
-        <Content />
+    <div className="site">
+      <div className="site-content">
+        <Router>
+          <div className="App">
+            <Header />
+            <Routes>
+              <Route path="/React-Portfolio" element={<About />} />
+              <Route path="/" element={<About />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/resume" element={<Resume />} />
+            </Routes>
+          </div>
+        </Router>
       </div>
-    </Router>
-    </div>
-        <Footer />
+      <Footer />
     </div>
   );
 }
